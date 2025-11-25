a81111f637fc3c767c07bb11079208a6d118dc4c
AventurasnaToita <aventurasnatoita@gmail.com>
2025-11-25 20:23:55 +0000

    Implement routing with React Router in App component

--- App.jsx
+++ App.jsx
@@ -1,15 +1,27 @@
-import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Catalog from "./pages/Catalog";
+import Content from "./pages/Content";
+import Profile from "./pages/Profile";
+import Help from "./pages/Help";
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-      <Home />
+      <main>
+        <Routes>
+          <Route path="/" element={<Início />} />
+          <Route path="/catalogo" element={<Catálogo />} />
+          <Route path="/content/:id" element={<Conteúdo />} />
+          <Route path="/perfil" element={<Perfil />} />
+          <Route path="/ajuda" element={<Ajuda />} />
+        </Routes>
+      </main>
       <Footer />
-    </>
+    </Router>
   );
 }
-
 export default App;
