bb746ce1ed563c16e509d8366907f5ea49bd97c0
AventurasnaToita <aventurasnatoita@gmail.com>
2025-11-25 20:28:56 +0000

    Replace static links with React Router Links

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,15 +1,16 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
   return (
     <nav className="navbar">
       <div className="site-logo">AnTPlay+</div>
       <ul className="nav-links">
-        <li>Início</li>
-        <li>Catálogo</li>
-        <li>Minha Lista</li>
-        <li>Perfil</li>
-        <li>Ajuda</li>
+        <li><Link to="/">Início</Link></li>
+        <li><Link to="/catalogo">Catálogo</Link></li>
+        <li><Link to="/perfil">Perfil</Link></li>
+        <li><Link to="/ajuda">Ajuda</Link></li>
       </ul>
-      <button className="login-btn">Entrar</button>
+      <Link to="/perfil"><button className="login-btn">Iniciar sessão</button></Link>
     </nav>
   );
 }
