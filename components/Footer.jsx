5f14092eafcc847495efb0a38906dfcc8cb4692f
AventurasnaToita <aventurasnatoita@gmail.com>
2025-11-25 20:15:34 +0000

    Add Footer component with copyright and links

---
+++ components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>© {new Date().getFullYear()} 2025 AnTPlay+</p>
+      <div className="footer-links">
+        <a href="#">Sobre Nós</a> | <a href="#">Privacidade</a> | <a href="#">Política de utilização</a> | <a href="#">Redes sociais</a>
+      </div>
+    </footer>
+  );
+}
+export default Footer;
